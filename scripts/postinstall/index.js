e21ea35df4772e322eddb1fe91560220d49381c9
Sushil Thakur <tsusheel.135@gmail.com>
2026-07-18 15:14:34 -0400

    post install script added

---
+++ scripts/postinstall/index.js
@@ -0,0 +1,14 @@
+console.log(`
+✨ Thank you for installing CleanrJS!
+
+🚀 Quick Start
+
+import Cleanr from "cleanrjs";
+
+Cleanr.validate.email("hello@gmail.com");
+
+📚 Documentation:
+https://github.com/tsusheel/cleanrjs
+
+❤️ If you like CleanrJS, please leave a GitHub star!
+`);
